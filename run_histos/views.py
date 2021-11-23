adf4e8ac55f20a40f4526d54be7e9692d42087a5
Xavier Coubez <xavier.coubez@cern.ch>
2021-11-23 18:44:44 +0100

    Adding first test.

---
+++ run_histos/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseRedirect
+
+from run_histos.models import RunHisto
+from runs.models import Run
+from django_tables2 import SingleTableMixin
+from dataset_tables.tables import RunHistosTable1D
+from run_histos.filters import RunHistosFilter1D
+from django_filters.views import FilterView
+from run_histos.utilities.utilities import request_contains_filter_parameter
+
+from .utils import get_altair_chart
+
+import pandas as pd
+import altair as alt
+
+# Create your views here.
+
+
+def listRunHistos1D(request):
+    """
+    View to list all 1D histograms for Run based data
+    """
+    return listRunHistos1DView.as_view()(request=request)
+
+class listRunHistos1DView(SingleTableMixin, FilterView):
+    table_class = RunHistosTable1D
+    model = RunHisto
+    template_name = "run_histos/listRunHistos1D.html"
+    filterset_class = RunHistosFilter1D
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        runHistos_list = RunHisto.objects.all()[:200]
+        runHistos_table = RunHistosTable1D(runHistos_list)
+        context["runHistos_table"] = runHistos_table
+    
+        return context
+
+def import_view(request):
+    return render(request, 'run_histos/import.html')
+
+def run_histos_view(request):
+
+    error_message = None
+    dataset       = None
+    variable      = None
+    chart_type    = None
+    df            = None
+    mean          = None
+    chart         = {}
+
+    # objects.all().values() provides a dictionary while objects.all().values_list() provides a tuple
+    runs_df      = pd.DataFrame(Run.objects.all().values())
+    runhistos_df = pd.DataFrame(RunHisto.objects.all().values())
+
+    if runhistos_df.shape[0] > 0:
+        df = pd.merge(runs_df, runhistos_df, left_on='id', right_on='run_number_id').drop(['id_x', 'id_y', 'run_number_id', 'date_x', 'date_y'], axis=1)
+
+        if request.method == 'POST':
+            dataset   = request.POST['dataset']
+            variable  = request.POST['variable']
+            chart_type = request.POST['plot_type']
+            print(f"dataset: {dataset} / variable: {variable} / chart_type: {chart_type}")
+
+        #df = df.query('primary_dataset.str.lower()==@dataset & title.str.lower()==@variable')
+        df = df.query('primary_dataset==@dataset & title==@variable')
+        mean = df['mean'].to_frame().to_html()
+
+        chart = get_altair_chart(chart_type, df=df)
+
+    else:
+        error_message = "No runhistos in the database"
+
+    context = {
+        'error_message': error_message,
+        'df':            df,
+        'mean':          mean,
+        'chart' :        chart,
+    }
+
+    return render(request, 'run_histos/main.html', context)
+
+def altair_chart_view(request):
+
+    chart = {}
+
+    runhistos_df = pd.DataFrame(RunHisto.objects.all().values()).head(100)
+
+    if runhistos_df.shape[0] > 0:   
+        chart_obj = alt.Chart(runhistos_df).mark_bar().encode(
+            x='mean',
+        ).to_json(indent=None)
+
+    else:
+        print("No runshistos in the database")
+
+    return JsonResponse(chart_obj)
--- runs/tests.py
+++ runs/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Run
 
 # Create your tests here.
+class RunModelTest(TestCase):
+
+    def test_saving_and_retrieving_run(self):
+        run = Run(run_number=1712)
+        run.save()
