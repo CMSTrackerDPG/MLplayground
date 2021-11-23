87d8df14ba61e2502e9d7c9ed9a5087d65b739c5
Xavier Coubez <xavier.coubez@cern.ch>
2021-11-23 19:28:09 +0100

    Adding functional test and first run tests

--- runs/tests.py
+++ runs/tests.py
@@ -1,7 +1,22 @@
 from django.test import TestCase
+from django.urls import resolve
+from runs.views import runs_view, run_view
+
 from .models import Run
 
 # Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_resolve_runs_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, runs_view)
+
+    def test_resolve_run_view(self):
+        found = resolve('/run/')
+        self.assertEqual(found.func, run_view)
+
+
 class RunModelTest(TestCase):
 
     def test_saving_and_retrieving_run(self):
